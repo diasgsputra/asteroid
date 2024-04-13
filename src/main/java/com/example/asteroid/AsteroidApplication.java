55b5aca08c3d87abdc2613c1290e94d6797c295c
DS23666X <diasgsputra@gmail.com>
2024-04-13 10:09:37 +0700

    add swagger and downgrade spring version

--- src/main/java/com/example/asteroid/AsteroidApplication.java
+++ src/main/java/com/example/asteroid/AsteroidApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableWebMvc
+@EnableSwagger2
 public class AsteroidApplication {
 
 	public static void main(String[] args) {
